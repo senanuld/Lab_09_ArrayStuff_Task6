89d409ade31f7eb27f1c6b3ef1cb70df6bb3147d
Leonard Senanu <senanuld@mail.uc.edu>
2025-11-03 12:07:22 -0500

    Lab_09_ArrayStuff_Task5.iml

---
+++ src/SafeInput.java
@@ -0,0 +1,20 @@
+public class SafeInput {
+    public static void main(String[] args) {
+
+        int[] dataPoints = new int[100];
+        int userValue = 0;
+
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userValue) {
+            }
+            count++;
+            System.out.println("The value " + userValue + " was found " + count + " times.");
+
+
+
+
+
+        }
+    }
+}
