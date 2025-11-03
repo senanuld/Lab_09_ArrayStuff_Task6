09a0f56078722edadc3d6224822a899f24c8d2b4
Leonard Senanu <senanuld@mail.uc.edu>
2025-11-03 12:55:48 -0500

    Lab_09_ArrayStuff_Task7

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -21,6 +21,27 @@ public class SafeInput {
             System.out.println("The value " + userValue + " was found " + count + " times.");
             System.out.println("The value " + userValue + " was not found " + count + " times.");
 
+            int min = dataPoints[0];
+            int max = dataPoints[dataPoints.length - 1];
+            for (int j = 1; j < dataPoints.length; j++) {
+                if (dataPoints[j] < min) {}
+                if (dataPoints[j] > max) {}
+                min = dataPoints[j];
+
+                System.out.println("Minimum value in the array is " + min);
+                System.out.println("Maximum value in the array is " + max);
+                System.out.println("Average value in the array is " + dataPoints[dataPoints.length - 1]);
+
+                int sum = 0;
+                for (int value : dataPoints) {
+                    sum += value;
+                }
+                
+
+
+            }
+
+
 
 
 
